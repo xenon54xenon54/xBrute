f1f47581afb0b210e6020b3ca2b5542d13967c77
xenon54xenon54 <34069040+xenon54xenon54@users.noreply.github.com>
2017-12-21 13:25:55 -0500

    Moved around some vars
    Now, simply type the sides of the equation into the first four variables respectively.

--- 2x1brute/clean.js
+++ 2x1brute/clean.js
@@ -1,10 +1,14 @@
+var side1_1 = /**/;
+var side1_2 = /**/;
+var side2_1 = /**/;
+var side2_2 = /**/;
 function test(x, y) {
-    return (((/*First side*/) == (/*Second side*/)) && ((/*First side*/) == (/*Second side*/)));
+    return (((side1_1) == (side1_2)) && ((side2_1) == (side2_2)));
 }
 var answers = 0;
-for (i = -20; i < 20; i++) {
+for (i = -20; i < 20; i = i + 1) {
     var x = i;
-    for (n = -20; n < 20; n++) {
+    for (n = -20; n < 20; n = n + 1) {
         var y = n;
         if (test(x, y) == true) {
             console.log(x + "," + y);
