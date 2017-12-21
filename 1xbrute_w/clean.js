ed886f20307dcd953ceb35b0d616af3a16d250dc
xenon54xenon54 <34069040+xenon54xenon54@users.noreply.github.com>
2017-12-20 21:11:43 -0500

    Cleaned comment version
    Exactly like the original, but without comments.

---
+++ 1xbrute_w/clean.js
@@ -0,0 +1,12 @@
+function test(x) {
+    return ((/*first side*/) == (/*other side*/));
+}
+var start = 0;
+var max = 100;
+var increment = 1;
+for (i = start; i <= max; i+increment) {
+    if (test(i)) {
+        console.log(i);
+        break;
+    }
+}
