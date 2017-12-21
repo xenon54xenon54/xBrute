080bd56dbaa3b5a15eb5ee273b176d7c86248612
xenon54xenon54 <34069040+xenon54xenon54@users.noreply.github.com>
2017-12-20 21:26:48 -0500

    Fixed increments
    To match the commented.

--- 1xbrute_w/single-line.js
+++ 1xbrute_w/single-line.js
@@ -1,2 +1,1 @@
-function test(x) {return ((/*first side*/) == (/*other side*/));};var start = 0;var max = 100;var increment = 1;for (i = start; i <= max; i+increment) {if (test(i)) {console.log(i);break;}}
-//Does not work for some reason. Debugging in progress.
+function test(x) {return ((/*first side*/) == (/*other side*/));};for (i = 0; i < 100; i++) {if (test(i)) {console.log(i);break;}}
