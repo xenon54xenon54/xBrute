4c4b0c1e2380b2e364e154ac87f74d2678197a5e
xenon54xenon54 <34069040+xenon54xenon54@users.noreply.github.com>
2017-12-21 11:57:19 -0500

    Updated clean.js
    Got rid of some unnecessary calcs

--- 1x2brute/clean.js
+++ 1x2brute/clean.js
@@ -1,13 +1,13 @@
 function test(x) {
-    return ((/*Left side*/) == (/*Right side (usually 0)*/));
+    return ((x ** 2 - 7 * x - 18) == (0));
 }
 var answers = 0;
 for (i = -20; i <= 100; i++) {
     if (test(i)) {
         console.log(i);
         answers = answers + 1;
-    }
-    if (test(i) && answers == 2) {
-        break;
+        if (answers == 2) {
+            break;
+        }
     }
 }
