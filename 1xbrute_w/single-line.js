c63fb81bbbf540b2d2247ef1fe94de60c71f53b8
xenon54xenon54 <34069040+xenon54xenon54@users.noreply.github.com>
2017-12-21 08:28:03 -0500

    Removed  unnecessary whitespace
    Because the smaller the file, the better, right?

--- 1xbrute_w/single-line.js
+++ 1xbrute_w/single-line.js
@@ -1,1 +1,1 @@
-function test(x) {return ((/*first side*/) == (/*other side*/));};for (i = 0; i < 100; i++) {if (test(i)) {console.log(i);break;}}
+function test(x){return((/*first side*/)==(/*other side*/));};for(i=0;i<100;i++){if(test(i)){console.log(i);break;}}
