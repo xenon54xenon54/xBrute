4b931a2535603e9cc790d64d015ca79bdfe863ea
xenon54xenon54 <34069040+xenon54xenon54@users.noreply.github.com>
2017-12-21 12:32:41 -0500

    fixed dumb typo

--- 2x1brute/single-line.js
+++ 2x1brute/single-line.js
@@ -1,1 +1,1 @@
-function test(x,y){return(((/*First side*/)==(/*Second side*/))&&((/*First side*/)==(/*Second side*/)));}var answers=0;for(i=-20;i<20;i++){var x=i;for(n=-20;n<20;n++){vary=n;if(test(x,y)==true){console.log(x+","+y);answers=answers+1;if(answers==1){break;}}}}
+function test(x,y){return(((/*First side*/)==(/*Second side*/))&&((/*First side*/)==(/*Second side*/)));}var answers=0;for(i=-20;i<20;i++){var x=i;for(n=-20;n<20;n++){var y=n;if(test(x,y)==true){console.log(x+","+y);answers=answers+1;if(answers==1){break;}}}}
