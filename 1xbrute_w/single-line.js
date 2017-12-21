8a5c0b87a3f9f97b1a40e50c708d4eb79f00d8d8
xenon54xenon54 <34069040+xenon54xenon54@users.noreply.github.com>
2017-12-20 21:09:06 -0500

    Does not work for now.
    Trying to figure out why...

--- 1xbrute_w/single-line.js
+++ 1xbrute_w/single-line.js
@@ -1,1 +1,2 @@
 function test(x) {return ((/*first side*/) == (/*other side*/));};var start = 0;var max = 100;var increment = 1;for (i = start; i <= max; i+increment) {if (test(i)) {console.log(i);break;}}
+//Does not work for some reason. Debugging in progress.
